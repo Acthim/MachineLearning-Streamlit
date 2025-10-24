32711ab83793699239dd119d3bb508aefb5b3c05
Acthim <wesleydiegodantas@gmail.com>
2025-10-24 19:51:52 +0000

    Steps to bild a machinelearnig model

--- ModeloTreinamento.py
+++ ModeloTreinamento.py
@@ -1,22 +1,22 @@
-#cu
 import pandas as pd
 import joblib 
 import os
 from sklearn import model_selection, preprocessing, pipeline, linear_model, metrics
 
 #ETAPA 01 - CARREGAR ARQUIVOS
-def CarregarDados(caminho_do_arquivo = "ModeloTreinamento.py"):
+def CarregarDados(caminho_do_arquivo="historicoAcademico.csv"):
     try:
         if os.path.exists(caminho_do_arquivo):
-            df =pd.read.read_cdv(caminho_do_arquivo, enconding="latin1",sp=',')
+            df = pd.read_csv(caminho_do_arquivo, encoding="latin1", sep=',')
             print("O arquivo foi carregado corretamente")
-
             return df
         else:
-            print('O arquivo não foi encontrado')
+            print("O arquivo não foi encontrado")
             return None
-    
     except Exception as e:
-        print("Erro inesperado ao procurar o arquivo")
+        print(f"Erro inesperado ao procurar o arquivo: {e}")
         return None
-    
+
+dados=CarregarDados()
+print(dados)
+
